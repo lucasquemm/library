9ba0a49b281b7e6c1d6bafe84477d21d17dd7579
Lucas <lucasbcmoraes@gmail.com>
2021-07-01 19:18:35 -0300

    atualizacao da tela de exibicao dos livros,update do formulario

--- script.js
+++ script.js
@@ -9,26 +9,21 @@ let recebeLido = document.querySelector('#lido')
 
 let minhaBiblioteca = []
 
+let id = 0
+
 function Livro(titulo, autor, ano, lido) {
   this.titulo = titulo
   this.autor = autor
   this.ano = ano
   this.lido = lido
+  this.id = id++
 }
 
 function limparConteudo() {
   recebeTitulo.value = ''
   recebeAutor.value = ''
   recebeAno.value = ''
-  recebeLido.value = 'off'
-}
-
-function verificaCheckbox() {
-  if (recebeLido.checked) {
-    return (recebeLido.value = true)
-  } else {
-    return (recebeLido.value = false)
-  }
+  recebeLido.checked = false
 }
 
 function salvaLivroNaBiblioteca() {
@@ -37,18 +32,34 @@ function salvaLivroNaBiblioteca() {
       recebeTitulo.value,
       recebeAutor.value,
       recebeAno.value,
-      verificaCheckbox(recebeLido)
+      recebeLido.checked
     )
   )
 }
 
+function alteraStatus(livro) {
+  if (livro.lido == true) {
+    livro.lido = false
+    return console.log('status alterado', minhaBiblioteca[0])
+  } else {
+    livro.lido = true
+    return console.log('status alterado', minhaBiblioteca[0])
+  }
+}
+
+function removeLivroDaBiblioteca(id) {
+  minhaBiblioteca = minhaBiblioteca.filter((livroid) => livroid.id != id)
+}
+
 function mostraLivrosDaBiblioteca() {
+  listadorDeLivros.innerHTML = ''
   minhaBiblioteca.forEach((livro) => listaLivros(livro))
 }
 
 salvaLivro.addEventListener('click', function () {
   salvaLivroNaBiblioteca()
   limparConteudo()
+  mostraLivrosDaBiblioteca()
 })
 
 mostraLivros.addEventListener('click', function () {
@@ -75,14 +86,29 @@ function listaLivros(livro) {
   let livrBannerAutor = document.createElement('p')
   let livrBannerAno = document.createElement('p')
   let livrBannerLido = document.createElement('p')
+  let removeLivroBotao = document.createElement('button')
+  let alteraStatusLido = document.createElement('button')
 
   listadorDeLivros.appendChild(livrBannerTudo)
+  livrBannerTudo.style.border = '2px solid black'
   livrBannerTudo.appendChild(livrBannerTitulo)
-  livrBannerTitulo.textContent = `Titulo ${livro.titulo}`
+  livrBannerTitulo.textContent = `Titulo: ${livro.titulo}`
   livrBannerTudo.appendChild(livrBannerAutor)
   livrBannerAutor.textContent = `Autor: ${livro.autor}`
   livrBannerTudo.appendChild(livrBannerAno)
   livrBannerAno.textContent = ` Ano: ${livro.ano}`
   livrBannerTudo.appendChild(livrBannerLido)
   livrBannerLido.textContent = verificaLido(livro)
+  livrBannerTudo.appendChild(removeLivroBotao)
+  removeLivroBotao.textContent = 'Remove Livro'
+  removeLivroBotao.addEventListener('click', function () {
+    removeLivroDaBiblioteca(livro.id)
+    mostraLivrosDaBiblioteca()
+  })
+  livrBannerTudo.appendChild(alteraStatusLido)
+  alteraStatusLido.textContent = 'Altera Status'
+  alteraStatusLido.addEventListener('click', function () {
+    alteraStatus(livro)
+    mostraLivrosDaBiblioteca()
+  })
 }

bda2afbd903db0731759f845a593b4fa30a1659d
Lucas <lucasbcmoraes@gmail.com>
2021-06-30 21:32:57 -0300

    aperfeicoamento da div de exibicao dos livros e update no formulario de criacao dos elementos

--- script.js
+++ script.js
@@ -5,29 +5,41 @@ let mostraLivros = document.querySelector('#mostraLivros')
 let recebeTitulo = document.querySelector('#titulo')
 let recebeAutor = document.querySelector('#autor')
 let recebeAno = document.querySelector('#ano')
+let recebeLido = document.querySelector('#lido')
 
 let minhaBiblioteca = []
 
-function Livro(titulo, autor, ano) {
+function Livro(titulo, autor, ano, lido) {
   this.titulo = titulo
   this.autor = autor
   this.ano = ano
+  this.lido = lido
 }
 
 function limparConteudo() {
   recebeTitulo.value = ''
   recebeAutor.value = ''
   recebeAno.value = ''
+  recebeLido.value = 'off'
+}
+
+function verificaCheckbox() {
+  if (recebeLido.checked) {
+    return (recebeLido.value = true)
+  } else {
+    return (recebeLido.value = false)
+  }
 }
 
 function salvaLivroNaBiblioteca() {
   minhaBiblioteca.push(
-    new Livro(recebeTitulo.value, recebeAutor.value, recebeAno.value)
+    new Livro(
+      recebeTitulo.value,
+      recebeAutor.value,
+      recebeAno.value,
+      verificaCheckbox(recebeLido)
+    )
   )
-}
-
-Livro.prototype.infoLivro = function () {
-  return `Titulo do Livro: ${this.titulo} |Autor:${this.autor}| Ano:${this.ano}`
 }
 
 function mostraLivrosDaBiblioteca() {
@@ -43,14 +55,34 @@ mostraLivros.addEventListener('click', function () {
   mostraLivrosDaBiblioteca()
 })
 
-const livro1 = new Livro('teste', 'testador', 1000, 10)
-const livro2 = new Livro('teste2', 'testador2', 1900, 12)
-const livro3 = new Livro('teste3', 'testador3', 2000, 20)
+const livro1 = new Livro('teste', 'testador', 1000, true)
+const livro2 = new Livro('teste2', 'testador2', 1900, false)
+const livro3 = new Livro('teste3', 'testador3', 2000, false)
 
 minhaBiblioteca.push(livro1, livro2, livro3)
 
+function verificaLido(livro) {
+  if (livro.lido) {
+    return `Lido`
+  } else {
+    return `Nao lido`
+  }
+}
+
 function listaLivros(livro) {
-  let livrBanner = document.createElement('p')
-  livrBanner.textContent = livro.infoLivro()
-  listadorDeLivros.appendChild(livrBanner)
+  let livrBannerTudo = document.createElement('div')
+  let livrBannerTitulo = document.createElement('p')
+  let livrBannerAutor = document.createElement('p')
+  let livrBannerAno = document.createElement('p')
+  let livrBannerLido = document.createElement('p')
+
+  listadorDeLivros.appendChild(livrBannerTudo)
+  livrBannerTudo.appendChild(livrBannerTitulo)
+  livrBannerTitulo.textContent = `Titulo ${livro.titulo}`
+  livrBannerTudo.appendChild(livrBannerAutor)
+  livrBannerAutor.textContent = `Autor: ${livro.autor}`
+  livrBannerTudo.appendChild(livrBannerAno)
+  livrBannerAno.textContent = ` Ano: ${livro.ano}`
+  livrBannerTudo.appendChild(livrBannerLido)
+  livrBannerLido.textContent = verificaLido(livro)
 }
